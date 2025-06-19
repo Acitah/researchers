06d531cff7f21dc27e6497a66028189fd9939a3c
= <=>
2025-06-19 18:39:06 +0300

    added some files

--- members.js
+++ members.js
@@ -1,13 +1,5 @@
 
-function toggleInfo(clickedCard) {
-    
-    const allCards = document.querySelectorAll('.member-card');
-  
-    allCards.forEach(card => {
-        if (card !== clickedCard) {
-            card.classList.remove('active');
-        }
-    });
-
-    clickedCard.classList.toggle('active');
+function toggleInfo(cardElement) {
+    cardElement.classList.toggle('active');
 }
+
